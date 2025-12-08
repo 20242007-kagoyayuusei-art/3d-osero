ecb774e1be2465f01b97aaa82b33fb10236beed8
kagoya08 <20242007-kagoyayuusei@hcs.ac.jp>
2025-12-08 13:56:37 +0900

    できたらしい

--- HelloApp/obj/Debug/net10.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Debug/net10.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2541cc338a265e854b5fb5f29178cbef8e359e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461d94ea1c605ce5a6d64404f4b3dee99ba7ea6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
